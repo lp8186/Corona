642965bf7ff6f022d61221c992b008c4b3feaeb4
liadp <lp8186@bs.amalnet.k12.il>
2021-04-15 17:08:12 +0300

    Second

--- app/src/main/java/com/example/corona/NewStudent.java
+++ app/src/main/java/com/example/corona/NewStudent.java
@@ -12,6 +12,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -25,7 +26,8 @@ import static com.example.corona.FBref.refUsers;
 public class NewStudent extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     EditText name,lastName,place,date;
-    Spinner grade,gradeNum,condition;
+    Spinner grade,gradeNum;
+    Switch condition;
     String [] options1= {"grade","ז","ח","ט","י","יא","יב"};
     String [] options2= {"grade num","1","2","3","4","5","6","7","8","9"};
     String [] options3= { "can't get vaccinated", "get vaccinated once"};
@@ -57,7 +59,8 @@ public class NewStudent extends AppCompatActivity implements AdapterView.OnItemS
         date= (EditText) findViewById(R.id.date);
         grade= (Spinner) findViewById(R.id.grade);
         gradeNum= (Spinner) findViewById(R.id.gradeNum);
-        condition= (Spinner) findViewById(R.id.condition);
+        condition= (Switch) findViewById(R.id.condition);
+        condition.setChecked(false);
 
         adp = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, options1);
         grade.setAdapter(adp);
@@ -66,8 +69,6 @@ public class NewStudent extends AppCompatActivity implements AdapterView.OnItemS
         gradeNum.setAdapter(adp);
         gradeNum.setOnItemSelectedListener(this);
         adp= new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, options3);
-        condition.setAdapter(adp);
-        condition.setOnItemSelectedListener(this);
 
         place.setVisibility(View.INVISIBLE);
         date.setVisibility(View.INVISIBLE);
@@ -78,34 +79,34 @@ public class NewStudent extends AppCompatActivity implements AdapterView.OnItemS
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         if(parent==grade)
-            grade1=position;
+            grade1=position+6;
         if (parent==gradeNum)
-            gradeNum1=position+6;
-        if (parent==condition){
-            if(position==0){
-                condition1=false;
-                place.setVisibility(View.INVISIBLE);
-                date.setVisibility(View.INVISIBLE);
-            }
-            else{
-                condition1=true;
-                place.setVisibility(View.VISIBLE);
-                date.setVisibility(View.VISIBLE);
-            }
-        }
+            gradeNum1=position;
     }
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+    public void change(View view) {
+        if (condition1) {
+            condition1 = false;
+            place.setVisibility(View.INVISIBLE);
+            date.setVisibility(View.INVISIBLE);
+        }
+        else {
+            condition1 = true;
+            place.setVisibility(View.VISIBLE);
+            date.setVisibility(View.VISIBLE);
+        }
     }
     public void submit(View view) {
         if (name.getText().toString().equals(""))
             Toast.makeText(this, "Enter the name", Toast.LENGTH_SHORT).show();
         else if (lastName.getText().toString().equals(""))
             Toast.makeText(this, "Enter the last name", Toast.LENGTH_SHORT).show();
-        else if (grade1==0)
+        else if (grade1==6)
             Toast.makeText(this, "Chose the grade", Toast.LENGTH_SHORT).show();
-        else if (gradeNum1==6)
+        else if (gradeNum1==0)
             Toast.makeText(this, "Chose the grade num", Toast.LENGTH_SHORT).show();
         else {
             if (condition1) {
@@ -146,7 +147,7 @@ public class NewStudent extends AppCompatActivity implements AdapterView.OnItemS
                 });
                 vaccine2= new Vaccine("null","null");
                 vaccine1= new Vaccine("null","null");
-                student = new Users(name.getText().toString(), lastName.getText().toString(), grade1, gradeNum1,true,vaccine1, vaccine2);
+                student = new Users(name.getText().toString(), lastName.getText().toString(), grade1, gradeNum1,false,vaccine1, vaccine2);
                 refUsers.child(String.valueOf(position)).setValue(student);
                 name.setText("");
                 lastName.setText("");
